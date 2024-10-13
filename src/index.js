590efc2ff14edbc5deb0f53a9fea78445de7fa97
Kate465 <kategokeborate@gmail.com>
2024-10-13 14:10:53 +0100

    added functionality to select

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-function updateTime(event) {
+function updateTime() {
   let tokyoElement = document.querySelector("#tokyo");
   let tokyoDateElement = tokyoElement.querySelector(".date");
   let tokyoTimeElement = tokyoElement.querySelector(".time");
   let tokyoTime = moment().tz("Asia/Tokyo");
+
   tokyoDateElement.innerHTML = moment().format("MMMM Do YYYY");
   tokyoTimeElement.innerHTML = tokyoTime.format(
     "h:mm:ss [<small>] A [</small>]"
@@ -18,5 +19,27 @@ function updateTime(event) {
     "h:mm:ss [<small>] A [</small]"
   );
 }
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city" >
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+           <div class="time">${cityTime.format(
+             "h:mm:ss"
+           )} <small> ${cityTime.format("A")} </small> </div>
+        </div>
+        <a href ="/"> All cities </a>
+         `;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
+
 updateTime();
 setInterval(updateTime, 1000);
